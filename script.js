abce88839fba1016710f13d1f8fd5ce9e1fb97e0
EnzoPierucci <enzopierucci@gmail.com>
2024-01-23 12:24:34 +0100

    MILESTONE 2

--- script.js
+++ script.js
@@ -2,37 +2,56 @@ const teamMembers = [
     {
       nome: "Wayne Barnett",
       ruolo: "Founder & CEO",
-      foto: "wayne-barnett-founder-ceo.jpg"
+      foto: "img/wayne-barnett-founder-ceo.jpg"
     },
     {
       nome: "Angela Caroll",
       ruolo: "Chief Editor",
-      foto: "angela-caroll-chief-editor.jpg"
+      foto: "img/angela-caroll-chief-editor.jpg"
     },
     {
       nome: "Walter Gordon",
       ruolo: "Office Manager",
-      foto: "walter-gordon-office-manager.jpg"
+      foto: "img/walter-gordon-office-manager.jpg"
     },
     {
       nome: "Angela Lopez",
       ruolo: "Social Media Manager",
-      foto: "angela-lopez-social-media-manager.jpg"
+      foto: "img/angela-lopez-social-media-manager.jpg"
     },
     {
       nome: "Scott Estrada",
       ruolo: "Developer",
-      foto: "scott-estrada-developer.jpg"
+      foto: "img/scott-estrada-developer.jpg"
     },
     {
       nome: "Barbara Ramos",
       ruolo: "Graphic Designer",
-      foto: "barbara-ramos-graphic-designer.jpg"
+      foto: "img/barbara-ramos-graphic-designer.jpg"
     }
   ];
   
 
+  const teamContainer = document.getElementById("team-container");
+
   for (const member of teamMembers) {
-    console.log(`Nome: ${member.nome}, Ruolo: ${member.ruolo}, Foto: ${member.foto}`);
+    const memberInfo = document.createElement("div");
+  
+    // Nome
+    const nameElement = document.createElement("p");
+    nameElement.textContent = `Nome: ${member.nome}`;
+    memberInfo.appendChild(nameElement);
+  
+    // Ruolo
+    const roleElement = document.createElement("p");
+    roleElement.textContent = `Ruolo: ${member.ruolo}`;
+    memberInfo.appendChild(roleElement);
+  
+    // Foto
+    const imgElement = document.createElement("img");
+    imgElement.src = member.foto;
+    memberInfo.appendChild(imgElement);
+  
+    teamContainer.appendChild(memberInfo);
   }
   
